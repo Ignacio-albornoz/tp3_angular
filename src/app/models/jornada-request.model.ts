92b1357e8d46f19075fdf96c4dc77ba0448d487d
ignacio-albornoz <IgnacioG.Albornoz@gmail.com>
2023-09-16 13:56:38 -0300

    Se crearon modelos para jornadas

---
+++ src/app/models/jornada-request.model.ts
@@ -0,0 +1,7 @@
+export interface JornadaRequestModel{
+    idEmpleado: Number;
+    idConcepto: Number;
+    fecha: Date;
+    hsTrabajadas?: Number;
+    laborable: Boolean;
+}
---
+++ src/app/models/jornada-response.model.ts
@@ -0,0 +1,8 @@
+export interface JornadaResponseModel{
+    id: Number;
+    nroDocumento: Number;
+    nombreCompleto: String;
+    fecha: Date;
+    concepto: String;
+    hsTrabajadas?: Number;
+}
